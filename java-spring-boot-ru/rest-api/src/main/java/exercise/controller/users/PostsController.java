626b701b821b05f9bfc1a7b18354a6df268a4f76
@hexlet/cli <support@hexlet.io>
2025-05-01 23:07:59 +0300

    submit java-spring-boot-ru/rest-api

--- java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,21 +1,15 @@
 package exercise.controller.users;
 
+import exercise.Data;
+import exercise.model.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-import exercise.Data;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import exercise.model.Post;
-
 // BEGIN
-@RestController("/api/users")
+@RestController
+@RequestMapping("/api/users")
 public class PostsController {
 
     private List<Post> posts = Data.getPosts();
